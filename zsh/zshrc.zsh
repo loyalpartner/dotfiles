6b1363e8c4b76014a5d55516ddc8ce2acb80dc35
lee <loyalpartner@163.com>
2013-06-27 22:14:26 +0800

    修复zshrc配置的bug

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -5,18 +5,18 @@ export EDITOR=$(which vim)
 export MAIL=loyalpartner@163.com
 export LANG="zh_CN.UTF-8"
 
-ZSH_THEME          = "ys"
-DISABLE_AUTO_TITLE = "true"
-plugins            = (git autojump archlinux web-search colored-man python node gem tmuxinator extract vi-mode myconfig xfce4)
-
+ZSH_THEME="ys"
+DISABLE_AUTO_TITLE="true"
+plugins=(git autojump archlinux web-search colored-man python node gem \
+  tmuxinator extract vi-mode myconfig xfce4)
 source $ZSH/oh-my-zsh.sh
 
 export PATH=$HOME/source/tmuxinator/bin:$PATH
+#function share(){ sudo mount.cifs $1 $2 -o user=GaoPP,pass=a }
 
 alias tmux='tmux -2'
 alias mux='tmuxinator'
 alias .='cd ~/dotfiles'
 alias ins='sudo pacman -S'
 
-#function share(){ sudo mount.cifs $1 $2 -o user=GaoPP,pass=a }
 # vim:set ft=zsh foldmethod=marker foldenable:
