25dbf8a2d56f7994e9bb4f8f9251de52d88ee85a
lee <loyalpartner@163.com>
2013-06-30 13:53:43 +0800

    less语法高亮

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -7,16 +7,20 @@ export LANG="zh_CN.UTF-8"
 
 ZSH_THEME="ys"
 DISABLE_AUTO_TITLE="true"
-plugins=(git autojump archlinux web-search colored-man python node gem \
+plugins=( \
+  git autojump archlinux web-search colored-man colorize \
+  coffee cake python node gem \
   tmuxinator extract vi-mode myconfig xfce4)
 source $ZSH/oh-my-zsh.sh
 
-export PATH=$HOME/source/tmuxinator/bin:$PATH
+#export PATH=$HOME/source/tmuxinator/bin:$PATH
 #function share(){ sudo mount.cifs $1 $2 -o user=GaoPP,pass=a }
 
 alias tmux='tmux -2'
 alias mux='tmuxinator'
 alias .='cd ~/dotfiles'
 alias ins='sudo pacman -S'
+
+PAGER='less -X -M' export LESSOPEN="| $(which src-hilite-lesspipe.sh) %s" export LESS=' -R '
 
 # vim:set ft=zsh foldmethod=marker foldenable:
