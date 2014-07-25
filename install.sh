ace6ea363d98eff8e1864da2234e34d2d4248e40
lee <loyalpartner@163.com>
2014-07-25 15:47:15 +0800

    增加选择ssh or http的功能

--- install.sh
+++ install.sh
@@ -3,12 +3,28 @@ script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 git_clone()
 {
-    if git clone https://github.com/loyalpartner/dotfiles.git dotfiles >> /dev/null
+    
+    echo 'choose ssh or https:'
+    echo '1) ssh'
+    echo '2) https'
+    read url_no
+    
+    case url_no in
+        1)
+            url=git@github.com:loyalpartner/dotfiles.git
+            ;;
+        *)
+            url=https://github.com/loyalpartner/dotfiles.git
+            ;;
+    esac
+
+    if git clone $url dotfiles > /dev/null
     then
         cd dotfiles
         cat setup.sh | sh
     fi
 }
 
-[ -d dotfiles ] && rm dotfiles.bak ||  mv dotfiles dotfiles.bak -fv && git_clone
+[ -d dotfiles ] && mv dotfiles dotfiles.bak -fv 
+git_clone
 
