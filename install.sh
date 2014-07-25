f2f9479c3a6841c3e1bc93d06ae8cecd950ec6e2
lee <loyalpartner@163.com>
2014-07-25 15:36:14 +0800

    install 修改

--- install.sh
+++ install.sh
@@ -3,13 +3,12 @@ script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 git_clone()
 {
-    if git clone https://github.com/loyalpartner/dotfiles.git dotfiles
+    if git clone https://github.com/loyalpartner/dotfiles.git dotfiles >> /dev/null
     then
         cd dotfiles
         cat setup.sh | sh
     fi
 }
 
-[ -d dotfiles ] && mv dotfiles dotfiles.bak -fv
-git_clone
+[ -d dotfiles ] && rm dotfiles.bak ||  mv dotfiles dotfiles.bak -fv && git_clone
 
--- setup.sh
+++ setup.sh
@@ -4,19 +4,19 @@ script_dir="$( cd "$( dirname "$0" )" && pwd )"
 echo $script_dir
 
 # 如果存在～/.zshrc先备份
-[ -e ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak -v
+[ -h ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak -v
 ln -s $script_dir/zsh/zshrc.zsh ~/.zshrc
 
 # 如果存在～/.tmux.conf先备份
-[ -e ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.bak -v
+[ -h ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.bak -v
 ln -s $script_dir/tmux/tmux.conf ~/.tmux.conf
 
 # 如果存在 ~/.vimperatorrc先备份
-[ -e ~/.vimperatorrc ] && mv ~/.vimperatorrc ~/.vimperatorrc.bak -v
+[ -h ~/.vimperatorrc ] && mv ~/.vimperatorrc ~/.vimperatorrc.bak -v
 ln -s $script_dir/vimperator/vimperatorrc.vim ~/.vimperatorrc
 
 # 如果存在 ~/.pentadactylrc先备份
-[ -e ~/.pentadactylrc ] && mv ~/.pentadactylrc ~/.pentadactylrc.bak -v
+[ -h ~/.pentadactylrc ] && mv ~/.pentadactylrc ~/.pentadactylrc.bak -v
 ln -s $script_dir/pentadactyl/pentadactylrc.vim ~/.pentadactylrc
 
 #if [[ ! -a ~/.w3m/keymap ]]; then
