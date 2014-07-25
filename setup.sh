d261ce7806a3e7dde6d75ca5415f9ca0f0cad7c2
lee <loyalpartner@163.com>
2014-07-25 16:37:19 +0800

    readme 说明

--- setup.sh
+++ setup.sh
@@ -11,13 +11,16 @@ ln -s $script_dir/zsh/zshrc.zsh ~/.zshrc
 [ -h ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.bak -v
 ln -s $script_dir/tmux/tmux.conf ~/.tmux.conf
 
-# 如果存在 ~/.vimperatorrc先备份
-[ -h ~/.vimperatorrc ] && mv ~/.vimperatorrc ~/.vimperatorrc.bak -v
-ln -s $script_dir/vimperator/vimperatorrc.vim ~/.vimperatorrc
+## 如果存在 ~/.vimperatorrc先备份
+## 已升级到 pentadactyl
+#[ -h ~/.vimperatorrc ] && mv ~/.vimperatorrc ~/.vimperatorrc.bak -v
+#ln -s $script_dir/vimperator/vimperatorrc.vim ~/.vimperatorrc
 
 # 如果存在 ~/.pentadactylrc先备份
 [ -h ~/.pentadactylrc ] && mv ~/.pentadactylrc ~/.pentadactylrc.bak -v
 ln -s $script_dir/pentadactyl/pentadactylrc.vim ~/.pentadactylrc
+
+# wget https://j.mp/spf13-vim3 -O ~/spf13-vim.sh && sh ~/spf13-vim.sh
 
 #if [[ ! -a ~/.w3m/keymap ]]; then
   #ln -s $script_dir/w3m/keymap ~/.w3m/keymap
