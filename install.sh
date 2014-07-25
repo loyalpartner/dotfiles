eac1c7b48824e5c503d036b0bedb34e562c4cb1b
lee <loyalpartner@163.com>
2014-07-25 15:16:26 +0800

    简化在线安装代码

--- install.sh
+++ install.sh
@@ -10,20 +10,6 @@ git_clone()
     fi
 }
 
-if [ -d dotfiles ]
-then
-
-    echo 'dotfiles alread exists:'
-    echo '是否备份(Y|N)：'
-
-    read CONFIRM
-
-    if [ "X$CONFIRM" = "Xy" ] || [ "X$CONFIRM" = "XY" ]
-    then
-        mv dotfiles dotfiles.bak -fv
-        git_clone
-    fi
-else
-    git_clone
-fi
+[ -d dotfiles ] && mv dotfiles dotfiles.bak -fv
+git_clone
 
