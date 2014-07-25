909c291c151b6dab70bd641d238608c7612ef78f
lee <loyalpartner@163.com>
2014-07-25 15:03:25 +0800

    在线安装-小修改

--- install.sh
+++ install.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
-if git clone https://github.com/loyalpartner/dotfiles.git dotfiles
+git_clone()
+{
+    if git clone https://github.com/loyalpartner/dotfiles.git dotfiles
+    then
+        cd dotfiles
+        cat setup.sh | sh
+    fi
+}
+
+if [ -d dotfiles ]
 then
-    cd dotfiles
-    ./setup.sh
+
+    echo 'dotfiles alread exists:'
+    echo '是否备份(Y|N)：'
+
+    read CONFIRM
+
+    if [ "X$CONFIRM" = "Xy" ] || [ "X$CONFIRM" = "XY" ]
+    then
+        mv dotfiles dotfiles.bak -fv
+        git_clone
+    fi
+else
+    git_clone
 fi
+
