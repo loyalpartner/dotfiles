dbbb0e4fd3e5d47c22a5c7d938e666544e9c2148
lee <loyalpartner@163.com>
2020-06-12 16:17:13 +0800

    auto check ostype.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ script_dir="$( cd "$( dirname "$0" )" && pwd )"
 echo $script_dir
 
 ln -fs $script_dir/zsh/zshrc.zsh ~/.zshrc
-ln -fs $script_dir/sxhkd $HOME/.config/sxhkd
 
-[[ "$OSTYPE" == "linux-gnu"* ]] && ln -fs xprofile $HOME/.xprofile
+if [[  "$OSTYPE" == "linux-gnu"*  ]]; then
+  ln -fs $script_dir/sxhkd $HOME/.config/sxhkd
+  ln -fs xprofile $HOME/.xprofile
+fi
 
