7c6dd9aa4f728439a872e751e0d3113284e19f94
lee <loyalpartner@163.com>
2020-06-16 09:40:20 +0800

    fix sxhkd install bug.

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
-echo $script_dir
-
-ln -fs $script_dir/zsh/zshrc.zsh ~/.zshrc
+ln -vfs $script_dir/zsh/zshrc.zsh ~/.zshrc
 
 if [[  "$OSTYPE" == "linux-gnu"*  ]]; then
-  ln -fs $script_dir/sxhkd $HOME/.config/sxhkd
-  ln -fs xprofile $HOME/.xprofile
+  if [[ ! -h  "$HOME/.config/sxhkd" ]]; then
+    ln -vfs $script_dir/sxhkd $HOME/.config/sxhkd/
+  fi
+  ln -vfs $script_dir/xprofile $HOME/.xprofile
 fi
 
