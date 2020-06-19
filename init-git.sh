5d8e4561b7f820e23f53a30b3243e12c43f89635
lee <loyalpartner@163.com>
2020-06-19 08:44:53 +0800

    git init config

---
+++ init-git.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+git config --global user.name lee
+git config --global user.email loyalpartner@163.com
+git config --global credential.helper store
+git config --global core.eol lf
+git config --global core.autocrlf false
+git config --global core.safecrlf true
+git config --global http.proxy socks5://127.0.0.1:1080
