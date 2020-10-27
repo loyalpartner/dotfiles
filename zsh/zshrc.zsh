eba2053d01ce7aafe143e79e9928bd8a26a0f39e
lee <loyalpartner@163.com>
2020-10-27 15:47:52 +0800

    add fzf.

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -76,7 +76,7 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git autojump pip npm history)
+plugins=(git autojump pip npm history fzf)
 
 source $ZSH/oh-my-zsh.sh
 
