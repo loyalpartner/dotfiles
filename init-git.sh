c28c6515e21e60623cd562fbddd1da2430c48835
lee <loyalpartner@163.com>
2021-01-15 08:47:21 +0800

    优化窗口切换逻辑

--- init-git.sh
+++ init-git.sh
@@ -8,4 +8,5 @@ git config --global core.autocrlf false
 git config --global core.safecrlf true
 git config --global core.excludesFile ~/.gitignore
 git config --global http.https://github.com.proxy socks5://127.0.0.1:1080
+git config --global forge.gitee.com/api/v5.user bbblili
 # git config --global http.proxy socks5://127.0.0.1:1080
--- sxhkd/switcher.sh
+++ sxhkd/switcher.sh
@@ -2,6 +2,7 @@
 
 # wid=$(xdotool search --class $1 | tail -1)
 
+# v1
 # if [[ -z "$wid" ]]; then 
 #   nohup $1 &
 # elif [[ "$wid" == "$(xdotool getactivewindow)" ]]; then
@@ -10,5 +11,21 @@
 #   xdotool windowactivate $wid 
 # fi
 
+# v2
 # [[ -z "$wid" ]] && nohup $1 || [[ "$wid" == "$(xdotool getactivewindow)" ]] && xdotool windowminimize $wid || xdotool windowactivate $wid 
-xdotool search --desktop "$(xdotool get_desktop)" --onlyvisible --class $1 windowactivate || $1 &
+
+# v3
+# xdotool search --desktop "$(xdotool get_desktop)" --onlyvisible --class $1 windowactivate || $1 &
+
+
+# v4
+desktop="$(xdotool get_desktop)"
+awid="$(xdotool getactivewindow)" # 获取当前窗口id
+nwid="$(xdotool search --desktop $desktop --onlyvisible --class $1 | tail -1)" # 获取上次 xxx 程序使用的窗口id
+
+# 上次使用的窗口是活动窗口，选择该程序的下一个窗口
+if [[ $awid == $nwid  ]]; then
+  xdotool search --desktop "$(xdotool get_desktop)" --onlyvisible --class $1 windowactivate || $1 &
+else
+  xdotool windowactivate $nwid
+fi
