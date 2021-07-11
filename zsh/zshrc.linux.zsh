a3bd4250f028ce5121cd709b5c5b5a69b5eda7aa
lee <loyalpartner@163.com>
2021-07-11 10:19:21 +0800

    feat: true color

--- zsh/zshrc.linux.zsh
+++ zsh/zshrc.linux.zsh
@@ -6,6 +6,7 @@ export PATH=~/.yarn/bin:$PATH
 # xset r rate 300 40
 source ~/Documents/work/dmpp/chromium/src/tools/bash-completion
 
+
 export FZF_DEFAULT_OPTS="--bind 'f1:execute(less -f {}),ctrl-y:execute-silent(echo {} |sed \"s/[0-9 ]\+//\" | xclip -selection clipboard)+abort'"
 
 alias gite="git config -e --global"
@@ -17,6 +18,7 @@ alias ml="mpc listall"
 alias mls="mpc ls"
 alias mm="mpc toggle"
 alias mt="mpc repeat 1;mpc toggle"
+alias ll="locate --database /var/lib/mlocate/chromium.db"
 
 alias xclip="xclip -selection clipboard"
 alias myip="echo $(curl -s http://myip.ipip.net)"
