23c01a0d8f402c47c224ee377791bdde71d949c6
lee <loyalpartner@163.com>
2021-10-31 18:06:05 +0800

    fix soft link to ctags.d

--- setup.sh
+++ setup.sh
@@ -15,29 +15,27 @@ install_softs(){
   fi
 }
 
-install_softs
-
+if [[ ! -d $HOME/.oh-my-zsh ]]
+then
+  install_softs
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 link_dot_configs(){
   ln -vfs $script_dir/zsh/zshrc.zsh ~/.zshrc
   ln -vfs $script_dir/tmux.conf ~/.tmux.conf
 }
 
 link_config_dir(){
-  [[ -h "$2" ]] || ln -vfs $script_dir/$1 $2
+  [[ -h "$2" ]] || ln -vfs $1 $2
 }
 
-link_config_dir "$script_dir/.ctags.d" "$HOME/.ctags.d"
-link_config_dir "$script_dir/sway" "$HOME/.config/sway"
-link_config_dir "$script_dir/alacritty" "$HOME/.config/alacritty"
-
+# link_config_dir $script_dir/ctags.d/ $HOME/.ctags.d
+cp -rf $script_dir/ctags.d $HOME/.ctags.d
+link_config_dir $script_dir/sway/ $HOME/.config/sway
+link_config_dir $script_dir/alacritty/ $HOME/.config/alacritty
+link_dot_configs
 
 source "$script_dir/init-git.sh"
 
-if [[ ! -d $HOME/.oh-my-zsh ]]
-then
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-fi
-
-link_dot_configs
