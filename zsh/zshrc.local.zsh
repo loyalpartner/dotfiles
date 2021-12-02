0d1062059ad47a659c42a38ef284f7ba36b92ed9
lee <loyalpartner@163.com>
2021-12-02 16:13:23 +0800

    use xtern insteadof alacritty

--- zsh/zshrc.local.zsh
+++ zsh/zshrc.local.zsh
@@ -17,6 +17,8 @@ export CLASSPATH=".:/usr/local/lib/antlr-4.9-complete.jar:$CLASSPATH"
 alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.9-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
 alias grun='java -Xmx500M -cp "/usr/local/lib/antlr-4.9-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
 
+alias trans="trans :zh"
+
 export PATH=/usr/lib/icecream/bin:$PATH
 
 # alias cdg="cd \\$(git rev-parse --show-toplevel)"
