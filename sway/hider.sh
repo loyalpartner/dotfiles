61f53e374a45fe9b433e0f693895485994e525e9
lee <loyalpartner@163.com>
2022-01-24 19:09:05 +0800

    update sway

--- sway/hider.sh
+++ sway/hider.sh
@@ -1,6 +1,9 @@
-# swaymsg -t get_workspaces | jq '.[] | select(.focused==true).num'
-SELECTOR='.. | select(.type?=="floating_con" and .visible==true).pid'
-PIDS="$(swaymsg -t get_tree | jq "$SELECTOR" )"
+# current workspace name
+WORKSPACE_NAME=$(swaymsg -t get_workspaces |
+  jq '.[] | select(.focused==true).name')
+S1=".. | select(.name?==$WORKSPACE_NAME and .type==\"workspace\")"
+S2='.. | select(.type?=="floating_con" and .visible==true).pid'
+PIDS="$(swaymsg -t get_tree | jq "$S1 | $S2")"
 
 for _ in $PIDS; do
   swaymsg "scratchpad show"
