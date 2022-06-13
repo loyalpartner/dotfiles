ee44bffc15e2ad54ffc15b1f191b202262835cfe
lee <loyalpartner@163.com>
2022-06-13 09:35:09 +0800

    update

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -120,4 +120,4 @@ fi
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-source /usr/share/nvm/init-nvm.sh
+[[ -f /usr/share/nvm/init-nvm.sh ]] && source /usr/share/nvm/init-nvm.sh
