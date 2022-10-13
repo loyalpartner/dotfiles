a821042124252994f61f5928db7eff1f567529cb
lee <loyalpartner@163.com>
2022-10-13 08:49:20 +0000

    update

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -15,7 +15,12 @@ export ZSH="${HOME}/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="powerlevel10k/powerlevel10k"
+if [[ "$(tty)" =~ "tty" ]]
+then
+  ZSH_THEME="robbyrussell"
+else
+  ZSH_THEME="powerlevel10k/powerlevel10k"
+fi
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -81,6 +86,7 @@ plugins=(
   autojump 
   pip 
   npm 
+  nvm
   lxd
   ansible
   history 
