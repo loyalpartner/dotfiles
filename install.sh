d361c91977310c12dd9415d97d947153417faa58
lee <loyalpartner@163.com>
2022-12-26 14:45:18 +0800

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-git clone https://github.com/loyalpartner/dotfiles.git $HOME/dotfiles &&
-  bash $HOME/dotfiles/setup.sh -- all
+git clone https://github.com/loyalpartner/dotfiles.git $HOME/dotfiles
+bash $HOME/dotfiles/setup.sh all
 
